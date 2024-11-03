c88642f7cdeabbcff6e6e8bc5f10cce96a485be0
Vatsal <vatsaldarji11@gmail.com>
2024-11-04 01:49:10 +0530

    implemented to handle any amount of numbers and different delimiters

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -11,3 +11,11 @@ test("returns number itself for single number", () => {
 test("returns sum of number for string of numbers", () => {
   expect(stringCalculator.add("1,2")).toBe(3);
 });
+
+test("returns sum of number for string of numbers when numbers have in new line", () => {
+  expect(stringCalculator.add("1\n2\n3")).toBe(6);
+});
+
+test("returns sum of number for string of numbers when numbers have in new line", () => {
+  expect(stringCalculator.add("//;\n1;2")).toBe(3);
+});
--- stringCalculator.ts
+++ stringCalculator.ts
@@ -2,7 +2,15 @@ export class stringCalculator {
   static add(numbers: string): number {
     if (!numbers) return 0;
 
-    const nums = numbers.split(",").map(Number);
+    let delimiter = /,|\n/;
+
+    if (numbers.startsWith("//")) {
+      const match = numbers.match(/^\/\/(.+)\n/);
+      delimiter = new RegExp(`[${match![1]}]`);
+      numbers = numbers.slice(match![0].length);
+    }
+
+    const nums = numbers.split(delimiter).map(Number);
     return nums.reduce((sum, n) => sum + n, 0);
   }
 }
